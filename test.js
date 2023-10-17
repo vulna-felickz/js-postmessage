225618eb992eba04591a7ecc8fd2fa12b4634d8a
Chad Bentz <1760475+felickz@users.noreply.github.com>
2023-10-17 15:59:32 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,20 @@
- window.parent.postMessage('reloadPage', '*');
+window.parent.postMessage(password, '*'); // NOT OK
+
+(function() {
+  var data = {};
+  data.foo = password;
+  data.bar = "unproblematic";
+
+  window.parent.postMessage(data, '*');     // NOT OK
+  window.parent.postMessage(data.foo, '*'); // NOT OK
+  window.parent.postMessage(data.bar, '*'); // OK
+})();
+
+window.parent.postMessage(authKey, '*');
+
+
+
+window.parent.postMessage('reloadPage', '*');
 
 
 window.console = window.console || function(t) {};
